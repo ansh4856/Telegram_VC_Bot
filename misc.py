cadeff2ab68c78b1e303a19748f1b6e0c7623bcd
ansh4856 <85155321+ansh4856@users.noreply.github.com>
2021-06-16 17:44:38 +0530

    Update misc.py

--- misc.py
+++ misc.py
@@ -5,7 +5,11 @@ HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 **/play** __youtube/saavn/deezer Song_Name__
 **/telegram** __Play From Telegram Audio.__
 **/pause** __Pause Music.__
-**/resume** __Resume Music.__"""
+**/resume** __Resume Music.__
+
+**🚧🚧Warning! Dont abuse the bot in anyways and play songs relavent to all tag @icecrac34r if anyone is causing chaos. He will be unauthorized🚧🚧**
+
+"""
 
 REPO_TEXT = (
     "[Github](https://github.com/thehamkercat/Telegram_vc_bot)"
