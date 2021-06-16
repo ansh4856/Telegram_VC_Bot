d12f2757805e0f0b69fddb7631c19fbdc5b4712e
ansh4856 <85155321+ansh4856@users.noreply.github.com>
2021-06-16 17:38:59 +0530

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,11 +1,11 @@
 HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 
 **/skip** __Skip The Current Playing Music.__
+**/queue** __List the queued songs.__
 **/play** __youtube/saavn/deezer Song_Name__
 **/telegram** __Play From Telegram Audio.__
 **/pause** __Pause Music.__
-**/resume** __Resume Music.__
-**/theme** __Change Curently Playing Theme.__"""
+**/resume** __Resume Music.__"""
 
 REPO_TEXT = (
     "[Github](https://github.com/thehamkercat/Telegram_vc_bot)"
